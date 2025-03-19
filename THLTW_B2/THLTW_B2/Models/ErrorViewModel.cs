cc3906e0d0b89db25cd816e9dee33456b7d26a91
cuthanhcam <cuthanhcam04@gmail.com>
2025-03-19 23:21:27 +0700

    Remove sample code

--- THLTW_B2/THLTW_B2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace THLTW_B2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- THLTW_B2/THLTW_B2/Repositories/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using THLTW_B2.Models;
-
-namespace THLTW_B2.Repositories
-{
-    public interface ICategoryRepository
-    {
-        IEnumerable<Category> GetAllCategories();
-    }
-}
